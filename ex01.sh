6b680cf61641b41e9ce6bc10bf005b8b8b4f7336
ananda <anandaheino2@gmail.com>
2021-04-26 15:08:53 -0300

    corrigindo o arquivo ex01.sh

--- ex01.sh
+++ ex01.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'Descobrir a quantiade de atrasos de uma dada companhia passada como parâmetro'
-cat data-aula-4 | cut -d, -f14,15 | grep '.,.' | wc -l $1
+cat data-aula-4 | cut -d, -f15,16 | grep '.,.' | wc -l $1
