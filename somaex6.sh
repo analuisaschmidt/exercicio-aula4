41c5283121e7196f1c891da1bb3fb9c3d2f35857
Igor Nieto <igoriraburonieto@hotmail.com>
2021-04-27 10:19:09 -0300

    Resolução exercício 07

--- somaex6.sh
+++ somaex6.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cat data-aula-4 | sort -n -t, -k16,17 | cut -d, -f16,17 | grep -v - | grep 'JFK' | cut -d, -f1 > colunas.txt
+
 echo 'vamos fazer o sum e vai dar certo: '
 
-awk '{SUM += $1} END {print SUM}' exemplo6.txt
+awk '{SUM += $1} END {print SUM}' colunas.txt
+
+
+
+
